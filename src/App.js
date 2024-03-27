e794f7080b2eb3575311693312ceecd90b48854e
bhandariamey <bhandariamey@gmail.com>
2024-03-27 14:57:25 +0530

    error handling

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
    try{ const response = await fetch("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json");
     const respData = await response.json();
     setData(respData);}catch(e){
-      console.log(e)
+      alert("fetchDataFailed")
     }
   };
 
